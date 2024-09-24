aace17f68196930f53c922f858f36ff2267afdd0
Aleksei Petrov <peta0982@gmail.com>
2024-09-24 23:21:41 +0500

    implement liquibase (create table, populate users)

--- src/main/java/ru/petrov/springjwt/model/dto/SignInRequest.java
+++ src/main/java/ru/petrov/springjwt/model/dto/SignInRequest.java
@@ -9,12 +9,12 @@ import lombok.Data;
 @Schema(description = "Запрос на аутентификацию")
 public class SignInRequest {
 
-    @Schema(description = "Имя пользователя", example = "Ivan")
+    @Schema(description = "Имя пользователя", example = "admin")
     @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
     @NotBlank(message = "Имя пользователя не может быть пустыми")
     private String username;
 
-    @Schema(description = "Пароль", example = "password")
+    @Schema(description = "Пароль", example = "admin")
     @Size(min = 8, max = 255, message = "Длина пароля должна быть от 8 до 255 символов")
     @NotBlank(message = "Пароль не может быть пустыми")
     private String password;
